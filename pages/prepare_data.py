d700240964fdfeec998a4156bf0aead3fd29d939
cheerzhang <zhanglenus@gmail.com>
2023-10-17 09:40:15 +0200

    Update prepare_data.py

--- pages/prepare_data.py
+++ pages/prepare_data.py
@@ -42,79 +42,56 @@ def app():
         df_gender = df_gender.rename(columns={first_name_column: 'first_name'})
         df_gender_filtered = df_gender[~df_gender['first_name'].isin(existing_first_names)]
         df_gender_filtered = df_gender_filtered[df_gender_filtered['first_name'].str.len() > 2]
-        df_gender_filtered['gender'] = False
-        df_gender_unknown = df_gender_filtered
-        df_gender_for_girl = df_gender_filtered
-        df_gender_for_boy = df_gender_filtered
         st.write(f"data to fill: {df_gender_filtered.shape}")
+        df_gender_filtered['female'] = False
+        df_gender_filtered['male'] = False
+        df_gender_filtered['unknown'] = False
 
-        # fill in the label
-        col_unknown, col_f, col_m = st.columns(3)
-        with col_unknown:
-            st.markdown("Label for Unknown")
-            df_unknown = st.data_editor(
-                df_gender_unknown[['first_name', 'gender']],
+        col_label, col_table = st.columns([3,1])
+        with col_label:
+            df_label = st.data_editor(
+                df_gender_filtered[['first_name', 'unknown', 'female', 'male']],
                 column_config={
-                    "gender": st.column_config.CheckboxColumn(
-                        "Unknown?",
-                        help="Select gender",
+                    "unknown": st.column_config.CheckboxColumn(
+                        "unknown?",
+                        help="unknown?",
                         default=False,
-                    )
-                },
-                disabled=['first_name'],
-                hide_index=True,
-                key = 'for_unknown'
-            )
-            df_unknown_ = df_unknown[df_unknown['gender']==True]
-            st.write(f"Unknowns have {df_unknown_.shape}")
-        with col_f:
-            st.markdown("Label for Female")
-            df_girl = st.data_editor(
-                df_gender_for_girl[['first_name', 'gender']],
-                column_config={
-                    "gender": st.column_config.CheckboxColumn(
+                    ),
+                    "female": st.column_config.CheckboxColumn(
                         "Female?",
-                        help="Select gender",
+                        help="Female?",
+                        default=False,
+                    ),
+                    "male": st.column_config.CheckboxColumn(
+                        "male?",
+                        help="male?",
                         default=False,
                     )
                 },
                 disabled=['first_name'],
                 hide_index=True,
-                key = 'for_girl'
-            ) 
-            df_girl_ = df_girl[df_girl['gender']==True]
-            st.write(f"girls have {df_girl_.shape}")
-        with col_m:
-            st.markdown("Label for Male")
-            df_boy = st.data_editor(
-                df_gender_for_boy[['first_name', 'gender']],
-                column_config={
-                    "gender": st.column_config.CheckboxColumn(
-                        "Male?",
-                        help="Select gender",
-                        default=False,
-                    )
-                },
-                disabled=['first_name'],
-                hide_index=True,
-                key = 'for_boy'
-            ) 
-            df_boy_ = df_boy[df_boy['gender']==True]
-            st.write(f"boys have {df_boy_.shape}")
-        df_unknown_['gender_code'] = 'U'
-        df_girl_['gender_code'] = 'F'
-        df_boy_['gender_code'] = 'M'
-        result_df = pd.concat([df_girl_, df_boy_, df_unknown_], axis=0)
-        result_df = result_df[['first_name', 'gender_code']]
+                key = 'for_label'
+            )
+        with col_table:
+            df_girl_ = df_label[df_label['female']==True]
+            df_boy_ = df_label[df_label['male']==True]
+            df_unknown_ = df_label[df_label['unknown']==True]
+            df_unknown_['gender_code'] = 'U'
+            df_girl_['gender_code'] = 'F'
+            df_boy_['gender_code'] = 'M'
+            result_df = pd.concat([df_girl_, df_boy_, df_unknown_], axis=0)
+            result_df = result_df[['first_name', 'gender_code']]
         if df_exist is not None:
             df_exist_file_ = df_exist[['first_name', 'gender_code']]
             combine_df = pd.concat([result_df, df_exist_file_], axis=0)
             st.write(f"new file size: from:green[{df_exist_file_.shape}] to :green[{combine_df.shape}]")
-            st.dataframe(combine_df)
+            with col_table:
+                st.dataframe(combine_df)
             download_csv(combine_df, 'train_gender')
         else:
             st.write(f"new file size: :green[{result_df.shape}]")
-            st.dataframe(result_df)
+            with col_table:
+                st.dataframe(result_df)
             download_csv(result_df, 'train_gender')
 
 
