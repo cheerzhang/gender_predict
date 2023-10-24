4cbbd144861a3bb7c3d9f13a360f0210b99dcc2f
cheerzhang <zhanglenus@gmail.com>
2023-10-24 13:08:49 +0200

    fix type of session

--- pages/model_predict_test.py
+++ pages/model_predict_test.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 st.session_state['run_id']= ''
-st.seeeion_state['model_option']= 'Logistic'
+st.session_state['model_option']= 'Logistic'
 def model_on_change():
     st.session_state['run_id']= ''
 
@@ -40,10 +40,10 @@ def app():
     else:
         df = None
     model_options = st.selectbox('Choose Model', ('Logistic', 'NN', 'CatBoost'), key='model_options')
-    if st.seeeion_state['model_option'] != model_options:
-        st.seeeion_state['model_option'] = model_options
+    if st.session_state['model_option'] != model_options:
+        st.session_state['model_option'] = model_options
         model_on_change()
-    run_id = st.text_input('RUN ID', st.seeeion_state['run_id'])
+    run_id = st.text_input('RUN ID', st.session_state['run_id'])
     
     if model_options == 'Logistic':
         if run_id == '' and df is not None:
